1272ce9a73e2cb82cd1fb295f425339615538fec
LongHalo <youremail@example.com>
2025-09-06 09:12:20 +0700

    final updateSemester

--- src/pages/semester/routes.tsx
+++ src/pages/semester/routes.tsx
@@ -5,7 +5,7 @@ import { Navigate, Route } from 'react-router-dom';
 import { SemesterCreatePage } from './create/index';
 
 export const semesterRoutes = [
-  <Route path="/semesters" element={<Navigate to="/semesters/active" />} />,
+  <Route path="/semesters" element={<Navigate to="/semesters/list" />} />,
   <Route path="/semesters/list" element={<SemesterListPage />} />,
   <Route path="/semesters/create" element={<SemesterCreatePage />} />,
   <Route path="/semesters/:id/edit" element={<SemesterEditPage />} />,
