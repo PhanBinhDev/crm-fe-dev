52a4f7d63385ea67daf9382a336f77b559e44003
diemdiem123 <vuongthidiem100499@gmail.com>
2025-09-22 22:43:23 +0700

    update ui kanban board

--- src/pages/workspace/components/KanbanColumn.tsx
+++ src/pages/workspace/components/KanbanColumn.tsx
@@ -4,7 +4,6 @@ import ModalEditColumn from '@/components/modals/ModalEditColumn';
 import { ColorPicker } from '@/components/shared/ColorPicker';
 import { DragDropType } from '@/constants';
 import { useModal } from '@/hooks/useModal';
-import { hexToRgba } from '@/utils/formatter';
 import { SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { useUpdate } from '@refinedev/core';
@@ -55,7 +54,7 @@ const KanbanColumn = ({ id, stage, activities, allStages = [] }: KanbanColumnPro
   const style: React.CSSProperties = {
     transform: CSS.Transform.toString(transform),
     transition,
-    background: stage.color ? hexToRgba(stage.color, 0.1) : '#fafafa',
+    background: '#fff',
     border: '1px solid #eeeaeaff',
     borderRadius: 8,
     opacity: isDragging ? 0.5 : 1,
